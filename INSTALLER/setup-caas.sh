33f19223693019503e2d253962ea3b9c168ac0a7
GF aka zerohat <gf@zerohat.net>
2017-10-08 16:06:20 +0200

    Installer bug fixes

--- INSTALLER/setup-caas.sh
+++ INSTALLER/setup-caas.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #########
+FRESH="NO"                  
 BASE="/srv/ibm-caas2"
 ICP_RELEASE="2.1.0-beta-2"
 MASTER_IP="CHANGE-ME"       ## CE Edition doesnt allow HA-cluster
@@ -9,32 +10,46 @@ CLUSTER_DOMAIN="prodcluster.local"
 CLUSTER_NAME="caas01"
 COMPANY="newCO"
 PRODUCT="CaaS"
-DEBUG="TRUE"
+DEBUG="FALSE"               # Set it to true if you are new to the IBM ICP setup
 #########
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+#
 
 if [ "${MASTER_IP}" == "CHANGE-ME" ]; then
+  echo ""
+  echo -e "$COL_RED ## PLEASE, adjust base vars virst !! ## $COL_RESET"
   exit 1
-  printf "\n\n# PLEASE, adjust base vars virst !! ##\n\n"
 fi
 
+if [ "${FRESH}" == "YES" ]; then
 ### pre-req
 if [ ! -d "${BASE}" ]; then
   mkdir -p ${BASE}
 fi
 
 ### IBM ICP CE Install
+echo -e "$COL_MAGENTA ## Pull initial IBM ICP Installer Docker image.. $COL_RESET"
+
 docker pull ibmcom/icp-inception:${ICP_RELEASE}
 cd ${BASE}
 docker run -e LICENSE=accept \
 -v "$(pwd)":/data ibmcom/icp-inception:${ICP_RELEASE} cp -r cluster /data
 
+echo -e "$COL_MAGENTA ## Adjust necessary config in ${BASE}/cluster directory... $COL_RESET"
 ### configuration
 cd ${BASE}/cluster
 cp config.yaml config.yaml.ORIG
 cp /root/.ssh/id_rsa4096 ssh_key
 #
-echo "
-[master]
+echo "[master]
 ${MASTER_IP}
 
 [worker]
@@ -44,8 +59,7 @@ ${MASTER_IP}
 ${PROXY_IP}
 " >hosts
 #
-echo "
-network_cidr: 10.1.0.0/16
+echo "network_cidr: 10.1.0.0/16
 service_cluster_ip_range: 10.0.0.1/24
 #
 default_admin_user: admin
@@ -60,14 +74,23 @@ metering_enabled: true
 " >config.yaml
 #
 if [ "${DEBUG}" == "TRUE" ]; then
- exit 1
  printf "\n### OKAY. please verify all ICP configs and than proceed manual!\n"
+ exit 1 
 else
+ echo -e "$COL_MAGENTA ## NOW, we will start the installer, depending on you node, it will take at least 10min... $COL_RESET"
+ echo -e "$COL_MAGENTA ## YOU HAVE 10sec to interrupt now :-> $COL_RESET"
+ sleep 10
  docker run -e LICENSE=accept --net=host -t -v "$(pwd)":/installer/cluster \
  ibmcom/icp-inception:${ICP_RELEASE} install
+ fi
+
+ echo -e "$COL_YELLOW ## Be aware, the are a lot of docker images to be taken care of, so wait 10 more mins, before logging in to the web ui... $COL_RESET"
+ sleep 4
 fi
 
+
 ### addons
+echo -e "$COL_MAGENTA ## Installing necessary addons, like HELM, ISTIO, Kubectl... $COL_RESET"
 cd /tmp
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
--- INSTALLER/setup.sh
+++ INSTALLER/setup.sh
@@ -7,23 +7,36 @@ GIT="/srv/newco"
 RVM="2.4.0"
 TERRAFORM="0.10.7"
 ###########
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+#
 
 if [ "${HOST}" == "CHANGE-ME" ]; then
+  echo ""
+  echo -e "$COL_RED ## PLEASE, adjust base vars virst !! ## $COL_RESET"
   exit 1
-  printf "\n\n# PLEASE, adjust base vars virst !!\n"
 fi
 
 
 ### base ubuntu setup
-echo "${IP}  ${HOST}" >>/etc/hosts
-echo " ${HOST}" >/etc/hostname
-apt-get -y update && && apt-get upgrade && apt-get -y dist-upgrade
+#echo "${IP}  ${HOST}" >>/etc/hosts  ## FIX THIS!
+echo "${HOST}" >/etc/hostname
+#
+timedatectl set-ntp no
+#
+apt-get -y update && apt-get upgrade && apt-get -y dist-upgrade
 apt-get -y install build-essential apt-transport-https \
 ca-certificates curl software-properties-common \
 libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 \
 libsqlite3-dev libpcap-dev git-core autoconf curl zlib1g-dev \
-libxml2-dev libxslt1-dev libyaml-dev
-
+libxml2-dev libxslt1-dev libyaml-dev unzip zip ntp
 #
 if [ ! -f "/usr/bin/python" ]; then
   ln -s /usr/bin/python3 /usr/bin/python
@@ -33,6 +46,7 @@ echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable"
+apt-get -y update
 # docker
 apt-get -y install docker-ce
 echo '
@@ -70,4 +84,10 @@ source ~/.bashrc
 rvm install ${RVM}
 rvm use ${RVM} --default
 
+##
+echo ""
+echo -e "$COL_YELLOW ## SEEMS we are done :-> ## $COL_RESET"
+echo -e "$COL_GREEN ## PLEASE, reboot your server now and afterwards start setup-caas.sh ## $COL_RESET"
+echo ""
+# EOF
 
